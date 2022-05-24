2610cf05dd8a143a6f59d0dcb7204423427325d8
Zhamilia Begalieva <61488305+zhamiliab@users.noreply.github.com>
2022-05-24 13:18:50 -0700

    Update index.js

--- stringPatternSearch/index.js
+++ stringPatternSearch/index.js
@@ -3,7 +3,23 @@
 // stringPatternSearch("lollipop", "lol") --> 1
 // stringPatternSearch("lolol", "lol") --> 2
 
-function stringPatternSearch(text, pattern) {}
+function stringPatternSearch(text, pattern) {
+let count = 0;
+  
+  for(let i=0; i< text.length; i++){
+    for (let j=0; j < pattern.length; j++){
+      const patternChar = pattern[j];
+      if(patternChar !== text[i+j]) {
+        break;
+      }
+
+      if(j === pattern.length -1) {
+        count++
+      }
+    }
+  }
+  return count;
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
